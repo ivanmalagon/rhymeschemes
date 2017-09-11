2e2d1802dce2009689340033a6007c8b9e2925ee
Ivan Malagon <hacheka@gmail.com>
2017-09-11 23:21:48 +0200

    Set mobx strict use

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import { useStrict } from 'mobx';
 import App from './App.html';
+
+useStrict(true);
 
 window.app = new App({
 	target: document.body
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,13 @@
-import { observable } from 'mobx';
+import { observable, extendObservable, computed } from 'mobx';
 
 const store = observable({
  currentTime: 0
+});
+
+extendObservable(store, {
+  time: computed(function () {
+    return store.currentTime * 4;
+  })
 });
 
 window.store = store;
